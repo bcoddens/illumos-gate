72e3e9dafb908c3dd7f06a1c7c2b4fd3e68fe07a
Bart Coddens <bart.coddens@gmail.com>
2013-10-24 21:12:12 +0200

    Local Build files added

---
+++ building.sh
@@ -0,0 +1,4 @@
+rm -rf closed
+tar -xjpf on-closed-bins.i386.tar.bz2
+tar -xjpf on-closed-bins-nd.i386.tar.bz2
+./nightly.sh illumos.sh
---
+++ deploy.sh
@@ -0,0 +1,2 @@
+sudo ./usr/src/tools/scripts/onu \
+    -t nightly-`date +%Y-%m-%d` -d $PWD/packages/i386/nightly-nd
